93cedbd206e794e16da010b9154f25a11065ceea
pz1u <jiu0414a@gmail.com>
2026-08-03 19:20:29 +0900

    #13 Feat : 가상투자 계좌 시작 화면 및 설정 기능 구현

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import ProductsView from '@/views/ProductsView.vue'
 import VirtualInvestView from '@/views/VirtualInvestView.vue'
+import VirtualInvestStartView from '@/views/VirtualInvestStartView.vue'
 import LeaderboardView from '@/views/LeaderboardView.vue'
 import MyPageView from '@/views/MyPageView.vue'
 import SecurityDetailView from '@/views/SecurityDetailView.vue'
@@ -12,6 +13,12 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomeView },
     { path: '/products', name: 'products', component: ProductsView },
     { path: '/virtual', name: 'virtual', component: VirtualInvestView },
+    {
+      path: '/virtual/start',
+      name: 'virtual-start',
+      component: VirtualInvestStartView,
+      meta: { hideBottomTabBar: true },
+    },
     { path: '/leaderboard', name: 'leaderboard', component: LeaderboardView },
     { path: '/my', name: 'my', component: MyPageView },
     { path: '/securities/:pk', name: 'security-detail', component: SecurityDetailView },
